f3ca0afdadb6f874f1f095f353a2d4ada3534955
Shiv Ashish <san.mnnit11@gmail.com>
2019-10-01 11:41:09 +0530

    Adding First Test Case

---
+++ src/com.shivashish/test/java/udemy/ecommerce_tc_1.java
@@ -0,0 +1,42 @@
+package com.shivashish.test.java.udemy;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author : shiv.ashish@grofers.com
+ * Contact me on san.mnnit11@gmail.com or 8105234517 for any query
+ * file created on 2019-10-01
+ */
+
+public class ecommerce_tc_1 extends base {
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        AndroidDriver<AndroidElement> driver = capabilities();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("Hello");
+
+        driver.hideKeyboard();
+
+        driver.findElement(By.xpath("//*[@text='Female']")).click();
+
+        driver.findElement(By.id("android:id/text1")).click();
+
+
+        driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Argentina\"));");
+
+        //   driver.findElement(MobileBy.AndroidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().textMatches(\"" + containedText + "\").instance(0))"));
+
+        driver.findElement(By.xpath("//*[@text='Argentina']")).click();
+
+        driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+    }
+
+}
